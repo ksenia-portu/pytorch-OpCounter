7d1f7fddda6e7aeaeff4e55d6a8b79079ed1fef6
Ksenia Portu <ksenia.portu@gmail.com>
2024-03-28 14:08:17 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools.command.install import install
 
 readme = open("README.md").read()
 
-fp = open("thop/__version__.py", "r").read()
-VERSION = eval(fp.strip().split()[-1])
+# fp = open("thop/__version__.py", "r").read()
+# VERSION = eval(fp.strip().split()[-1])
 
 requirements = [
     "torch",
@@ -18,7 +18,8 @@ requirements = [
 # import subprocess
 # commit_hash = subprocess.check_output("git rev-parse HEAD", shell=True).decode('UTF-8').rstrip()
 # VERSION += "_" + str(int(commit_hash, 16))[:8]
-VERSION += "_" + datetime.datetime.now().strftime("%Y%m%d%H%M")[2:]
+#VERSION += "_" + datetime.datetime.now().strftime("%Y%m%d%H%M")[2:]
+VERSION = "0.1.1"
 print(VERSION)
 
 setup(
